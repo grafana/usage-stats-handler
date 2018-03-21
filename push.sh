5b9e3c8d096c2eb201c611bc2b50891d8d8e414a
bergquist <carl.bergquist@gmail.com>
2018-03-21 21:16:51 +0100

    push to /bergquist/ until I can create repos in grafana org

--- push.sh
+++ push.sh
@@ -1,7 +1,12 @@
 #/bin/sh
 VERSION=v1
 
-docker build -t grafana/usage-stats-handler:latest -t grafana/usage-stats-handler:$VERSION --no-cache=true .
+# docker build -t grafana/usage-stats-handler:latest -t grafana/usage-stats-handler:$VERSION --no-cache=true .
 
-docker push grafana/usage-stats-handler:latest
-docker push grafana/usage-stats-handler:$VERSION
+# docker push grafana/usage-stats-handler:latest
+# docker push grafana/usage-stats-handler:$VERSION
+
+docker build -t bergquist/usage-stats-handler:latest -t bergquist/usage-stats-handler:$VERSION --no-cache=true .
+
+docker push bergquist/usage-stats-handler:latest
+docker push bergquist/usage-stats-handler:$VERSION
