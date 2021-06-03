92b3d409d6771e4ab9b3498f46989685337d46d1
Dan Cech <dcech@grafana.com>
2021-06-03 11:57:48 -0400

    update dependencies, use current node lts, improve logging

--- usage-stats-handler/app.js
+++ usage-stats-handler/app.js
@@ -1,51 +1,64 @@
-var program = require('commander');
-var graphite = require('graphite');
-var pkg = require('./package.json');
-var _ = require('lodash');
-var elastic = require('./elastic');
+const program = require('commander');
+const graphite = require('graphite');
+const pkg = require('./package.json');
+const _ = require('lodash');
+const elastic = require('./elastic');
 
-var restify = require('restify');
-var server = restify.createServer({name: 'grafana-statsusage'});
+const restify = require('restify');
+const server = restify.createServer({name: 'grafana-usage-stats'});
+
+function parseIntOption(value) {
+  // parseInt takes a string and a radix
+  const parsedValue = parseInt(value, 10);
+  if (isNaN(parsedValue)) {
+    throw new program.InvalidOptionArgumentError('Not a number.');
+  }
+  return parsedValue;
+}
 
 program
   .version(pkg.version)
-  .option('-g, --graphite <graphite>', 'Graphite address')
-  .option('-e, --elastic <elastic>', 'Elastic address')
-  .option('--interval <seconds>', 'Interval in seconds');
+  .requiredOption('-g, --graphite <graphite>', 'Graphite address')
+  .requiredOption('-e, --elastic <elastic>', 'Elastic address')
+  .option('--interval <seconds>', 'Interval in seconds', parseIntOption, 600)
+  .parse();
 
-program.parse(process.argv);
+const options = program.opts();
 
-if (!program.graphite || !program.interval) {
-  program.outputHelp();
-  process.exit(1);
-}
-
-elastic.initElastic({ url: program.elastic });
-
-var graphiteUrl = 'plaintext://' + program.graphite;
-var intervalMs = parseInt(program.interval) * 1000;
-var prefix = "grafana.usagestats.";
+const graphiteUrl = 'plaintext://' + options.graphite;
+const elasticUrl = options.elastic;
+const intervalMs = parseInt(options.interval) * 1000;
+const prefix = "grafana.usagestats.";
 
 console.log('Graphite: ' + graphiteUrl);
+console.log('Elastic: ' + elasticUrl);
 console.log('Interval: ' + intervalMs);
+
+elastic.initElastic({ url: elasticUrl });
 
 server
   .use(restify.plugins.fullResponse())
   .use(restify.plugins.queryParser())
   .use(restify.plugins.bodyParser());
 
-var metrics = {};
+let metrics = {};
 
 function incrementCounter(name, amount) {
+  if (typeof amount != 'number') {
+    return false;
+  }
   value = metrics[name] || 0;
   value += amount;
   metrics[name] = value;
+  return true;
 }
 
 function sendMetrics() {
+  /*
   _.each(metrics, function(value, key) {
-    //console.log('key: ' + key + ' = ' + value);
+    console.log('key: ' + key + ' = ' + value);
   });
+  */
 
   var client = graphite.createClient(graphiteUrl);
   client.write(metrics, function(err) {
@@ -61,13 +74,13 @@ function sendMetrics() {
 setInterval(sendMetrics, intervalMs);
 
 server.post('/grafana-usage-report', function (req, res, next) {
-  var report = req.body;
+  const report = req.body;
+  console.log(JSON.stringify(report));
 
   elastic.saveReport(report);
-  console.log('report received: ', report);
 
-  var versionedPrefix = prefix + 'versions.' + report.version + '.';
-  var allPrefix = prefix + 'all.';
+  const versionedPrefix = prefix + 'versions.' + report.version + '.';
+  const allPrefix = prefix + 'all.';
 
   incrementCounter(versionedPrefix + 'reports.count', 1);
   incrementCounter(allPrefix + 'reports.count', 1);
@@ -84,7 +97,7 @@ server.use(function(err, req, res, next) {
   if (err) {
     console.error("error!", err.toString());
     console.error(err.stack);
-    res.status(500).send('Something broke!');
+    res.send(500, 'Something broke!');
   }
 });
 
--- usage-stats-handler/run.sh
+++ usage-stats-handler/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "graphite: ${GRAPHITE}"
 echo "elastic:  ${ELASTIC}"
