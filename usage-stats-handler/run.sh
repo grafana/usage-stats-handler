03feac5eb8770edc74394932371b71216003b446
Dan Cech <dcech@grafana.com>
2026-02-02 10:17:50 -0500

    update dependencies, remove unused elasticsearch client, switch to express (#46)

--- usage-stats-handler/run.sh
+++ usage-stats-handler/run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 echo "graphite: ${GRAPHITE}"
-echo "elastic:  ${ELASTIC}"
 
-node /usr/src/app/app.js -g ${GRAPHITE} -e ${ELASTIC} --interval 600
+node /usr/src/app/app.js -g ${GRAPHITE} --interval 600
